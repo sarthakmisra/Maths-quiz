580498868ea94b0b78b31a65e7d493d9a1b8b7c4
sarthakmisra <78964203+sarthakmisra@users.noreply.github.com>
2021-10-26 18:18:35 +0530

    Add files via upload

--- game_page.js
+++ game_page.js
@@ -17,14 +17,15 @@ function send() {
     number1 = document.getElementById("num1").value;
     number2 = document.getElementById("num2").value;
     actual_answer = parseInt(number1) * parseInt(number2);
-    question_number = "h4" + number1 + "X" + number2 + "</h4>";
+    question = "<h4>Q." + number1 + "X" + number2 + "=</h4>";
 input_box = "<br>Answer : <input type='number' id='input_checkbox'>";
 check_button = "<br><br><button class='btn btn-danger' onclick='check()'>Check</button>"
 row = question + input_box + check_button;
-}
-
 
 document.getElementById("resultado").innerHTML = row;
 
 document.getElementById("num1").value="";
 document.getElementById("num1").value="";
+}
+
+
