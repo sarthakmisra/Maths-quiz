a492a645e2a549cbafc22925ce0bd6ab2719596f
sarthakmisra <78964203+sarthakmisra@users.noreply.github.com>
2021-10-29 22:33:32 +0530

    Add files via upload

--- game_page.js
+++ game_page.js
@@ -19,7 +19,7 @@ function send() {
     actual_answer = parseInt(number1) * parseInt(number2);
     question = "<h4>Q." + number1 + "X" + number2 + "=</h4>";
 input_box = "<br>Answer : <input type='number' id='input_checkbox'>";
-check_button = "<br><br><button class='btn btn-danger' onclick='check()'>Check</button>"
+check_button = "<br><br><button class='btn btn-warning' onclick='check()'>Check</button>"
 row = question + input_box + check_button;
 
 document.getElementById("resultado").innerHTML = row;
@@ -27,5 +27,18 @@ document.getElementById("resultado").innerHTML = row;
 document.getElementById("num1").value="";
 document.getElementById("num1").value="";
 }
+function check() {
+    get_answer == document.getElementById("input_checkbox").value;
+    if (get_answer == actual_answer) {
+        if(answer_turn == "player1") {
+            update_player1_score = player1_score +1;
+            document.getElementById("player1_score").innerHTML = update_player1_score;
+        }
 
-
+        else 
+        {
+            update_player2_score = player2_score +1;
+            document.getElementById("player2_score").innerHTML = update_player2_score;  
+        }
+    }
+}
